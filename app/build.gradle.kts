bd4610aad5ff887bc42c0649d947cfb3d6934ed1
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-01-06 00:06:44 +0700

    Settup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.example.drugbank"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.drugbank"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
