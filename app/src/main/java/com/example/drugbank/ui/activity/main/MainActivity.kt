c7e825f72e3a5a2f6a2382012fa9a74854c3d3bc
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-01-06 00:06:56 +0700

    Settup

--- app/src/main/java/com/example/drugbank/ui/activity/main/MainActivity.kt
+++ app/src/main/java/com/example/drugbank/ui/activity/main/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.example.drugbank
+package com.example.drugbank.ui.activity.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.drugbank.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
