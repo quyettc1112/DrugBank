50e47304003af8b851f67dbd289925bd2889f76b
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-01-27 01:15:14 +0700

    Done UI User Management

--- app/src/main/java/com/example/drugbank/repository/UserAPIService.kt
+++ app/src/main/java/com/example/drugbank/repository/UserAPIService.kt
@@ -1,4 +1,4 @@
-package com.example.drugbank.data.api
+package com.example.drugbank.repository
 
 
 
@@ -10,11 +10,16 @@ import retrofit2.http.Header
 import retrofit2.http.Query
 
 interface UserAPIService {
-
     @GET("user/get-pageable-users")
     fun getPageableUser(
         @Header("Authorization") authorization: String,
-        @Query("gender") gender: Int
+        @Query("pageNo") pageNo: Int,
+        @Query("pageSize") pageSize: Int,
+        @Query("sortField") sortField: String,
+        @Query("sortOrder") sortOrder: String,
+        @Query("roleName") roleName: String?,
+        @Query("status") status: String?,
+        @Query("gender") gender: Int?,
     ): Call<UserListResponse>
 
 }
