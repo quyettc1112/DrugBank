c1bb5d2b601d978fe94233d83a7601052d56121c
QuyetTran1112 <133133145+QuyetTran1112@users.noreply.github.com>
2024-02-04 18:05:26 +0800

    Update Some UI Search

---
+++ app/src/main/java/com/example/drugbank/respone/DrugMListRespone.kt
@@ -0,0 +1,35 @@
+package com.example.drugbank.respone
+
+
+import com.google.gson.annotations.SerializedName
+import androidx.annotation.Keep
+import com.example.drugbank.data.model.Pageable
+import com.example.drugbank.data.model.Sort
+
+data class DrugMListRespone(
+ @SerializedName("content") val content: List<Content>,
+ @SerializedName("empty") val empty: Boolean,
+ @SerializedName("first") val first: Boolean,
+ @SerializedName("last") val last: Boolean,
+ @SerializedName("number") val number: Int,
+ @SerializedName("numberOfElements") val numberOfElements: Int,
+ @SerializedName("pageable") val pageable: Pageable,
+ @SerializedName("size") val size: Int,
+ @SerializedName("sort") val sort: Sort,
+ @SerializedName("totalElements") val totalElements: Int,
+ @SerializedName("totalPages") val totalPages: Int
+) {
+ data class Content(
+  @SerializedName("approvalStatus") val approvalStatus: Int,
+  @SerializedName("clinicalDescription") val clinicalDescription: String,
+  @SerializedName("description") val description: String,
+  @SerializedName("drugbankId") val drugbankId: Any,
+  @SerializedName("id") val id: Int,
+  @SerializedName("name") val name: String,
+  @SerializedName("simpleDescription") val simpleDescription: String,
+  @SerializedName("state")val state: String,
+  @SerializedName("type") val type: String
+ )
+
+
+}
