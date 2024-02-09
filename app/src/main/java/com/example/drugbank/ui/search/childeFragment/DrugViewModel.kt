9bb063842a17e3e0942f998750205a450fd4098a
QuyetTran1112 <133133145+QuyetTran1112@users.noreply.github.com>
2024-02-09 12:05:11 +0800

    Done Setup UI Sort by And Sort Field

--- app/src/main/java/com/example/drugbank/ui/search/childeFragment/DrugViewModel.kt
+++ app/src/main/java/com/example/drugbank/ui/search/childeFragment/DrugViewModel.kt
@@ -7,11 +7,34 @@ import javax.inject.Inject
 
 class DrugViewModel @Inject constructor() : ViewModel() {
 
-    val currentDrugList = MutableLiveData<List<Drug>>()
+    val currentDrugList = MutableLiveData<List<Drug>?>()
+    val selectedSortField = MutableLiveData<String>()
+    val selectefSortBy = MutableLiveData<String>()
+    val currentPage = MutableLiveData<Int>()
+
+    init {
+        selectedSortField.value = "id"
+        selectefSortBy.value = "asc"
+        currentPage.value = 0
+
+    }
 
     fun loadMoreDruglist(list: List<Drug>) {
         val currentList = currentDrugList.value ?: emptyList()
         val updatedList = currentList + list
         currentDrugList.value = updatedList
+    }
+
+    fun incrementCurrentPage() {
+        val currentValue = currentPage.value ?: 0 // Lấy giá trị hiện tại của currentPage, hoặc mặc định là 0 nếu chưa có giá trị
+        currentPage.value = currentValue + 1 // Gán giá trị mới cho currentPage là giá trị hiện tại cộng thêm 1
+    }
+
+    fun resetCurrentPage() {
+        currentPage.value = 0 // Gán giá trị mới cho currentPage là 0
+    }
+
+    fun emptyDrugList() {
+        currentDrugList.value = null
     }
 }
