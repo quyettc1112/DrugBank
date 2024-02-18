7239c1a3a0943c4b0d76bc9e020264ba0ce64a06
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-02-18 12:10:32 +0700

    Done Seach Drug Realtime

--- app/src/main/java/com/example/drugbank/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/drugbank/ui/search/SearchFragment.kt
@@ -7,13 +7,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.viewpager.widget.ViewPager
-import com.example.drugbank.R
 import com.example.drugbank.databinding.FragmentSearchBinding
-import com.example.drugbank.ui.search.childeFragment.DrugAdapter
-import com.example.drugbank.ui.search.childeFragment.DrugFragment
-import com.google.android.material.tabs.TabLayout
+import com.example.drugbank.ui.search.childeFragment.DrugFragment.DrugFragment
 
 class SearchFragment : Fragment() {
 
@@ -43,7 +39,7 @@ class SearchFragment : Fragment() {
 
     private fun setUpTabWithSearchView() {
 
-        val searchView = binding.searchView
+
 
 
         val sectionsPagerAdapter = SectionsPagerAdapter(childFragmentManager)
@@ -52,27 +48,7 @@ class SearchFragment : Fragment() {
         binding.viewPager.adapter = sectionsPagerAdapter
         binding.tabs.setupWithViewPager(binding.viewPager)
 
-        searchView.setOnQueryTextListener(object :  androidx.appcompat.widget.SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                if (!query.isNullOrEmpty()) {
-                    fragment.RESET_VIEWMODEL_VALUE()
-                    fragment._drugViewModel.searchField.value = query
-//                    fragment.CallDrugList()
-//                    fragment.TestFun()
 
-                    Log.d("CurrentSearchValue", fragment._drugViewModel.searchField.value.toString())
-                }
-
-                return true
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-
-                return true
-            }
-
-
-        })
 
 
         binding.viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
@@ -82,7 +58,7 @@ class SearchFragment : Fragment() {
 
             override fun onPageSelected(position: Int) {
                 if (position == 0) {
-                    searchView.setQuery("", false)
+
                 }
                 Log.d("CheckOnpaheSelect", position.toString())
             }
@@ -93,27 +69,9 @@ class SearchFragment : Fragment() {
         })
     }
 
-    private fun setUpControlSearchValue() {
 
 
-    }
 
-    private fun setUpSearchView() {
-        val searchView = binding.searchView
-        searchView.setOnQueryTextListener(object : androidx.appcompat.widget.SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                TODO("Not yet implemented")
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                TODO("Not yet implemented")
-            }
-
-
-        })
-
-
-    }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
