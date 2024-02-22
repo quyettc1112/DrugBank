3e5979b11b991356d9a29c46885863bd09b9dd1a
QuyetTran1112 <133133145+QuyetTran1112@users.noreply.github.com>
2024-02-22 16:55:03 +0700

    Update Logo

--- app/src/main/java/com/example/drugbank/common/constant/Constant.kt
+++ app/src/main/java/com/example/drugbank/common/constant/Constant.kt
@@ -1,12 +1,15 @@
 package com.example.drugbank.common.constant
 
+import android.content.Context
 import com.example.drugbank.R
 import com.example.drugbank.ui.drugstore.DrugStoreFragment
 
 class Constant {
     companion object {
+        const val USER_VALUE_SF = "USER_VALUE_SF"
         const val USERNAME_OR_EMAIL = "USERNAME_OR_EMAIL"
         const val USER_PASSWORD = "USER_PASSWORD"
+        const val USER_TOKEN = "USER_TOKEN"
 
         const val SEARCH_NAV_ID = 0;
         const val USER_MANAGER_NAV_ID = 3;
@@ -18,7 +21,7 @@ class Constant {
         const val DOCTOR_SECURITY_DOCTOR = "123"
         const val TOKEN_USER = "TOKEN_USER"
         const val ACCESS_TOKEN = "ACCESS_TOKEN"
-        const val REFESH_TOEKN = "REFESH_TOKEN"
+        const val REFESH_TOKEN = "REFESH_TOKEN"
 
         fun getNavSeleted(selected: Int): Int {
             val myHashMap = HashMap<Int, Int>()
@@ -30,41 +33,22 @@ class Constant {
             myHashMap[LOGIN_NAV_ID] = R.id.loginFragmentNav
             return myHashMap[selected] ?: -1
         }
-        fun getUserList():  ArrayList<tempUser>{
-            val itemList = ArrayList<tempUser>()
-            var user1 =tempUser(R.drawable.anh_2, "Trần Cương Quyết")
-            var user2 =tempUser(R.drawable.anh_3, "Trần Cương Quyết")
-            var user3 =tempUser(R.drawable.anh_4, "Trần Cương Quyết")
-            var user4 =tempUser(R.drawable.avatar_1, "Trần Cương Quyết")
-            itemList.add(user1)
-            itemList.add(user2)
-            itemList.add(user3)
-            //itemList.add(user4)
-            return itemList
-        }
-    }
 
-    class  tempUser(
-        val imageIcon: Int,
-        val nameIcon: String,
-        val actionId:Int? = null
-
-    )
-    sealed class MEDICAL() {
-        enum class INT(val range: IntRange, val dimension: String){
-            HEARTH_RATE(40..200, "bpm"),
-            BLOOD_SUGAR(40..200, "mg/dL")
-        }
-        enum class FLOAT(val range: ClosedFloatingPointRange<Float>, val dimension: String){
-            WEIGHT(0f..300f, "kg"),
-            HEIGHT(0f..300f, "cm"),
-            BODY_TEMPERATURE(34f..40f, "C")
+        fun getSavedUsername(context: Context): String? {
+            val sharedPreferences = context.getSharedPreferences(USER_VALUE_SF, Context.MODE_PRIVATE)
+            return sharedPreferences.getString(USERNAME_OR_EMAIL, null)
         }
 
-        enum class STRING(val regex: Regex, val dimension: String){
-            BLOOD_PRESSURE("""^\d{2,3}/\d{2,3}$""".toRegex(), "mg Hg")
+        fun getSavedPassword(context: Context): String? {
+            val sharedPreferences = context.getSharedPreferences(USER_VALUE_SF, Context.MODE_PRIVATE)
+            return sharedPreferences.getString(USER_PASSWORD, null)
         }
+
+
 
     }
+
+
+
 
 }
