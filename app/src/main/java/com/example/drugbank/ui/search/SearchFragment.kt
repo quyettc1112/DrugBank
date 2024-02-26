22c8cd665d039938c71ef8ff3086f2c198cb6590
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-02-26 20:03:12 +0700

    Updaate UI Product

--- app/src/main/java/com/example/drugbank/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/drugbank/ui/search/SearchFragment.kt
@@ -10,6 +10,7 @@ import android.view.ViewGroup
 import androidx.viewpager.widget.ViewPager
 import com.example.drugbank.databinding.FragmentSearchBinding
 import com.example.drugbank.ui.search.childeFragment.DrugFragment.DrugFragment
+import com.example.drugbank.ui.search.childeFragment.ProductFragment.ProductFragment
 
 class SearchFragment : Fragment() {
 
@@ -17,7 +18,7 @@ class SearchFragment : Fragment() {
     private var _binding: FragmentSearchBinding? = null
     private val binding get() = _binding!!
     private lateinit var viewModel: SearchViewModel
-    lateinit var fragment: DrugFragment
+    lateinit var fragment: ProductFragment
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,31 +40,12 @@ class SearchFragment : Fragment() {
 
     private fun setUpTabWithSearchView() {
 
-
-
         val sectionsPagerAdapter = SectionsPagerAdapter(childFragmentManager)
         val currentItem = binding.viewPager.currentItem
-        fragment = (sectionsPagerAdapter.instantiateItem(binding.viewPager, currentItem) as? DrugFragment)!!
+        fragment = (sectionsPagerAdapter.instantiateItem(binding.viewPager, currentItem) as? ProductFragment)!!
         binding.viewPager.adapter = sectionsPagerAdapter
         binding.tabs.setupWithViewPager(binding.viewPager)
 
-
-        binding.viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
-            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
-                // Không cần thực hiện gì ở đây
-            }
-
-            override fun onPageSelected(position: Int) {
-                if (position == 0) {
-
-                }
-                Log.d("CheckOnpaheSelect", position.toString())
-            }
-
-            override fun onPageScrollStateChanged(state: Int) {
-                // Không cần thực hiện gì ở đây
-            }
-        })
     }
 
 
--- app/src/main/java/com/example/drugbank/ui/search/childeFragment/ProductFragment/ProductFragment.kt
+++ app/src/main/java/com/example/drugbank/ui/search/childeFragment/ProductFragment/ProductFragment.kt
@@ -6,9 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.drugbank.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ProductFragment : Fragment() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
