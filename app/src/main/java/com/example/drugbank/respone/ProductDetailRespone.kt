1e3cf697cb0612a64eb88e8f847a2055d7ed45c7
QuyetTran1112 <133133145+QuyetTran1112@users.noreply.github.com>
2024-02-27 11:22:23 +0700

    Done Repository Product

---
+++ app/src/main/java/com/example/drugbank/respone/ProductDetailRespone.kt
@@ -0,0 +1,77 @@
+package com.example.drugbank.respone
+
+
+import com.google.gson.annotations.SerializedName
+import androidx.annotation.Keep
+
+
+data class ProductDetailRespone(
+ @SerializedName("authorities") val authorities: List<Any>,
+ @SerializedName("category") val category: Category,
+ @SerializedName("contraindication") val contraindication: Contraindication,
+ @SerializedName("drugIngredients") val drugIngredients: List<Any>,
+ @SerializedName("id") val id: Int,
+ @SerializedName("labeller") val labeller: String,
+ @SerializedName("manufactor") val manufactor: Manufactor,
+ @SerializedName("name") val name: String,
+ @SerializedName("pharmacogenomic") val pharmacogenomic: Pharmacogenomic,
+ @SerializedName("prescriptionName") val prescriptionName: String,
+ @SerializedName("productAllergyDetail") val productAllergyDetail: ProductAllergyDetail,
+ @SerializedName("route") val route: String
+) {
+ @Keep
+ data class Category(
+  @SerializedName("id")
+  val id: Int,
+  @SerializedName("slug")
+  val slug: String,
+  @SerializedName("title")
+  val title: String
+ )
+
+ @Keep
+ data class Contraindication(
+  @SerializedName("relationship")
+  val relationship: String,
+  @SerializedName("value")
+  val value: String
+ )
+
+ @Keep
+ data class Manufactor(
+  @SerializedName("company")
+  val company: String,
+  @SerializedName("countryId")
+  val countryId: Int,
+  @SerializedName("countryName")
+  val countryName: String,
+  @SerializedName("name")
+  val name: String,
+  @SerializedName("score")
+  val score: Any,
+  @SerializedName("source")
+  val source: String
+ )
+
+ @Keep
+ data class Pharmacogenomic(
+  @SerializedName("asorption")
+  val asorption: String,
+  @SerializedName("indication")
+  val indication: String,
+  @SerializedName("mechanismOfAction")
+  val mechanismOfAction: String,
+  @SerializedName("pharmacodynamic")
+  val pharmacodynamic: String,
+  @SerializedName("toxicity")
+  val toxicity: String
+ )
+
+ @Keep
+ data class ProductAllergyDetail(
+  @SerializedName("detail")
+  val detail: String,
+  @SerializedName("summary")
+  val summary: String
+ )
+}
