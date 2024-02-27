3a1056ca138341f81ba4c23c153eda091d363f0a
QuyetTran1112 <133133145+QuyetTran1112@users.noreply.github.com>
2024-02-27 11:22:14 +0700

    Done Repository Product

--- app/src/main/java/com/example/drugbank/repository/Admin_ProductM_Repository.kt
+++ app/src/main/java/com/example/drugbank/repository/Admin_ProductM_Repository.kt
@@ -1,4 +1,33 @@
 package com.example.drugbank.repository
 
-class Admin_ProductM_Repository {
+import dagger.hilt.android.scopes.ActivityScoped
+import java.net.IDN
+import javax.inject.Inject
+
+@ActivityScoped
+class Admin_ProductM_Repository @Inject constructor(
+    private val adminProductmRepository: Admin_ProductM_APIService
+) {
+
+    fun getProductList(
+        authorization: String,
+        pageNo: Int,
+        pageSize: Int,
+        sortField: String,
+        sortOrder: String,
+        search: String
+    ) = adminProductmRepository.getProductList(authorization, pageNo, pageSize, sortField, sortOrder, search)
+
+
+    fun getProductDetail(
+        authorization: String,
+        id: Int
+    ) = adminProductmRepository.getProductDetail(authorization, id)
+
+    fun deleteProduct(
+        authorization: String,
+        id: Int
+    )  =adminProductmRepository.deleteProduuct(authorization, id)
+
+
 }
