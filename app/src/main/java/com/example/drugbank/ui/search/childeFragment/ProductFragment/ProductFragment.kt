7dc27396c822d4ea151340922a6dcf4b43042e85
QuyetTran1112 <133133145+QuyetTran1112@users.noreply.github.com>
2024-02-27 07:56:44 +0000

    Done Set up UI

--- app/src/main/java/com/example/drugbank/ui/search/childeFragment/ProductFragment/ProductFragment.kt
+++ app/src/main/java/com/example/drugbank/ui/search/childeFragment/ProductFragment/ProductFragment.kt
@@ -37,19 +37,6 @@ class ProductFragment : Fragment() {
         return  _binding.root
     }
 
-    class GridSpacingItemDecoration(private val spacing: Int) : RecyclerView.ItemDecoration() {
-        override fun getItemOffsets(
-            outRect: Rect,
-            view: View,
-            parent: RecyclerView,
-            state: RecyclerView.State
-        ) {
-            outRect.left = spacing * 2
-            outRect.top = spacing
-            outRect.right = spacing * 2
-            outRect.bottom = spacing
-        }
-    }
 
 
 }
