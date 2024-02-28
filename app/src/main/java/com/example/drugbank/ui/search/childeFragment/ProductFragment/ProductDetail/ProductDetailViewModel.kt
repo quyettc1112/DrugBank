b7bbaf7b591a7b5b968362cd22f3f6b3c44cb8be
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-02-28 15:37:30 +0700

    Update UI Product Detail

---
+++ app/src/main/java/com/example/drugbank/ui/search/childeFragment/ProductFragment/ProductDetail/ProductDetailViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.drugbank.ui.search.childeFragment.ProductFragment.ProductDetail
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class ProductDetailViewModel @Inject constructor() : ViewModel() {
+    // TODO: Implement the ViewModel
+}
