8c7fee76fc5fa19307fa96b49981c7e60ce74710
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-03-02 00:32:53 +0700

    Done basic UI

--- app/src/main/java/com/example/drugbank/ui/search/childeFragment/ProductFragment/ProductDetail/ProductDetailViewModel.kt
+++ app/src/main/java/com/example/drugbank/ui/search/childeFragment/ProductFragment/ProductDetail/ProductDetailViewModel.kt
@@ -3,11 +3,14 @@ package com.example.drugbank.ui.search.childeFragment.ProductFragment.ProductDet
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.drugbank.respone.ProductDetailRespone
 import javax.inject.Inject
 
 class ProductDetailViewModel @Inject constructor() : ViewModel() {
 
     private var _isLoading = MutableLiveData<Boolean>()
+
+    val currentDetail = MutableLiveData<ProductDetailRespone?>()
 
     val isLoading: LiveData<Boolean>
         get() = _isLoading
@@ -15,5 +18,18 @@ class ProductDetailViewModel @Inject constructor() : ViewModel() {
     // Hàm này được gọi khi bạn muốn cập nhật trạng thái loading
     fun setLoading(isLoading: Boolean) {
         _isLoading.value = isLoading
+    }
+
+    init {
+        currentDetail.value = null
+    }
+
+    fun setCurrentProductDetail(productDetailRespone: ProductDetailRespone) {
+        currentDetail.value = productDetailRespone
+    }
+
+    fun resetCurrentProductDetail() {
+        currentDetail.value = null
+
     }
 }
