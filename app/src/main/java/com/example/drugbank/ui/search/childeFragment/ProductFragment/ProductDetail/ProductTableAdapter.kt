ec328c5c4e15f88538f872b18e58d19d1900ab3c
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-03-02 16:16:42 +0700

    Done UI Product Detail

--- app/src/main/java/com/example/drugbank/ui/search/childeFragment/ProductFragment/ProductDetail/ProductTableAdapter.kt
+++ app/src/main/java/com/example/drugbank/ui/search/childeFragment/ProductFragment/ProductDetail/ProductTableAdapter.kt
@@ -3,16 +3,20 @@ package com.example.drugbank.ui.search.childeFragment.ProductFragment.ProductDet
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.drugbank.R
-import com.example.drugbank.data.model.Drug
+import com.example.drugbank.data.model.DrugIn
 
-class ProductTableAdapter(private val data: ArrayList<String>):
+class ProductTableAdapter(private val data: List<DrugIn>):
     RecyclerView.Adapter<ProductTableAdapter.ViewHolder>() {
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-//        val textViewName: TextView = itemView.findViewById(R.id.textViewName)
-//        val textViewDetail: TextView = itemView.findViewById(R.id.textViewDetail)
+            val drugIn_Name: TextView = itemView.findViewById(R.id.drugIn_name)
+            val drugIn_strength: TextView = itemView.findViewById(R.id.drugIn_strength)
+            val drugIn_strengthNumber: TextView = itemView.findViewById(R.id.drugIn_strengthNumber)
+            val drugIn_strengthUnit: TextView = itemView.findViewById(R.id.drugIn_strengthUnit)
+            val drug_clinical: TextView = itemView.findViewById(R.id.drug_clinical)
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
@@ -23,8 +27,12 @@ class ProductTableAdapter(private val data: ArrayList<String>):
     override fun getItemCount(): Int = data.size
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-//        holder.textViewName.text = item.first
-//        holder.textViewDetail.text = item.second
+        holder.drugIn_Name.text = item.name
+        holder.drugIn_strength.text = item.strength
+        holder.drugIn_strengthUnit.text = item.strengthUnit
+        holder.drugIn_strengthNumber.text = item.strengthNumber
+        holder.drug_clinical.text = item.clinicallyRelevant
+
     }
 
 }
