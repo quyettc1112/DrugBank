94e078102983337e936a078f4efaee3b01b4d783
QuyetTran1112 <133133145+QuyetTran1112@users.noreply.github.com>
2024-03-05 06:30:39 +0000

    Done Product

--- app/src/main/java/com/example/drugbank/repository/Admin_ProductDetail_Service.kt
+++ app/src/main/java/com/example/drugbank/repository/Admin_ProductDetail_Service.kt
@@ -3,6 +3,7 @@ package com.example.drugbank.repository
 import com.example.drugbank.respone.ProductDetailRespone
 import dagger.hilt.android.scopes.ActivityScoped
 import retrofit2.Call
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
@@ -14,4 +15,10 @@ interface Admin_ProductDetail_Service {
         @Header("Authorization") authorization: String,
         @Query("id") id: Int,
     ): Call<ProductDetailRespone>
+
+    @DELETE("admin/approval-product-management/delete-approval-product")
+    fun deleteProduuct(
+        @Header("Authorization") authorization: String,
+        @Query("id") id: Int
+    ): Call<Void>
 }
