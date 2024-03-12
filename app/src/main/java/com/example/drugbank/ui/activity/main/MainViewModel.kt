49f56426245afc429866bc00a3275edd5a0c8afd
QuyetTran1112 <133133145+QuyetTran1112@users.noreply.github.com>
2024-03-12 11:15:19 +0700

    Done Profie

--- app/src/main/java/com/example/drugbank/ui/activity/main/MainViewModel.kt
+++ app/src/main/java/com/example/drugbank/ui/activity/main/MainViewModel.kt
@@ -1,15 +1,18 @@
 package com.example.drugbank.ui.activity.main
 
 import androidx.lifecycle.MutableLiveData
+import com.example.drugbank.respone.UserListResponse
 import com.example.healthcarecomp.base.BaseViewModel
 
 class MainViewModel: BaseViewModel() {
 
     var currentNav = MutableLiveData<Int?>()
     var testValue = MutableLiveData<Int>()
+    var currentUser = MutableLiveData<UserListResponse.User?>()
 
     init {
         testValue.value = 0
+        currentUser.value =  null
 
     }
     fun ChangeNav(IDNav: Int) {
@@ -17,6 +20,14 @@ class MainViewModel: BaseViewModel() {
     }
     fun setUpNav() {
         currentNav.value = 0
+    }
+
+    fun updateCurrentUser(user: UserListResponse.User?) {
+        currentUser.value = user
+    }
+
+    fun clearCurrentUser() {
+        currentUser.value = null
     }
 
 
