22a3e72d60832bd495f9d2d550c7dc9075bc8b50
QuyetTran1112 <133133145+QuyetTran1112@users.noreply.github.com>
2024-03-12 11:14:49 +0700

    Done Profie

--- app/src/main/java/com/example/drugbank/common/constant/Constant.kt
+++ app/src/main/java/com/example/drugbank/common/constant/Constant.kt
@@ -5,7 +5,9 @@ import com.example.drugbank.R
 import com.example.drugbank.data.model.Drug
 import com.example.drugbank.respone.ProductDetailRespone
 import com.example.drugbank.respone.ProductListRespone
+import com.example.drugbank.respone.UserListResponse
 import com.example.drugbank.ui.drugstore.DrugStoreFragment
+import org.json.JSONObject
 
 class Constant {
     companion object {
@@ -26,6 +28,9 @@ class Constant {
         const val TOKEN_USER = "TOKEN_USER"
         const val ACCESS_TOKEN = "ACCESS_TOKEN"
         const val REFESH_TOKEN = "REFESH_TOKEN"
+
+        const val CURRENT_USER = "CURRENT_USER"
+        const val CURRENT_USER_VALUE = "CURRENT_USER_VALUE"
 
 
         const val CURRENT_PRODUCT_ID = "CURRENT_PRODUCT_ID"
@@ -50,6 +55,47 @@ class Constant {
             return sharedPreferences.getString(USERNAME_OR_EMAIL, null)
         }
 
+        fun saveCurrentUser(context: Context, user: UserListResponse.User) {
+            val sharedPreferences = context.getSharedPreferences(CURRENT_USER, Context.MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+            val userJson = JSONObject()
+            userJson.put("id", user.id)
+            userJson.put("username", user.username)
+            userJson.put("email", user.email)
+            userJson.put("fullname", user.fullname)
+            userJson.put("dayOfBirth", user.dayOfBirth)
+            userJson.put("gender", user.gender)
+            userJson.put("roleName", user.roleName)
+            userJson.put("isActive", user.isActive)
+            if (user.avatar!= null) {
+                userJson.put("avatar", user.avatar)
+            } else  userJson.put("avatar", R.drawable.user_general)
+
+            editor.putString(CURRENT_USER_VALUE, userJson.toString())
+            editor.apply()
+        }
+
+        fun getCurrentUser(context: Context): UserListResponse.User? {
+            val sharedPreferences = context.getSharedPreferences(CURRENT_USER, Context.MODE_PRIVATE)
+            val userJsonString = sharedPreferences.getString(CURRENT_USER_VALUE, null)
+
+            return if (userJsonString != null) {
+                val userJson = JSONObject(userJsonString)
+                UserListResponse.User(
+                    userJson.getInt("id"),
+                    userJson.getString("username"),
+                    userJson.getString("email"),
+                    userJson.getString("fullname"),
+                    userJson.getString("dayOfBirth"),
+                    userJson.getInt("gender"),
+                    userJson.getString("roleName"),
+                    userJson.getString("isActive"),
+                    userJson.getString("avatar"),
+                )
+            } else {
+                null
+            }
+        }
         fun getSavedPassword(context: Context): String? {
             val sharedPreferences = context.getSharedPreferences(USER_VALUE_SF, Context.MODE_PRIVATE)
             return sharedPreferences.getString(USER_PASSWORD, null)
@@ -63,12 +109,22 @@ class Constant {
             editor.apply()
         }
 
+        fun removeAllCurrentUser(context: Context) {
+            val sharedPreferences = context.getSharedPreferences(CURRENT_USER, Context.MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+            sharedPreferences.all.keys.forEach { key ->
+                editor.remove(key)
+            }
+            editor.apply()
+        }
+
         fun saveUserCredentials(context: Context, username: String?, password: String?, token: String?) {
             val sharedPreferences = context.getSharedPreferences(USER_VALUE_SF, Context.MODE_PRIVATE)
             val editor = sharedPreferences.edit()
             editor.putString(USERNAME_OR_EMAIL, username)
             editor.putString(USER_PASSWORD, password)
             editor.putString(USER_TOKEN, token)
+
             editor.apply()
         }
 
