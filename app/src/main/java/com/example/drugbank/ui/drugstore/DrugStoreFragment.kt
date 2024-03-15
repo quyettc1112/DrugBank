caa2fff78cacc5c375ef4787916958e3e563acfc
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-03-16 00:18:25 +0700

    yeah đã có google map

--- app/src/main/java/com/example/drugbank/ui/drugstore/DrugStoreFragment.kt
+++ app/src/main/java/com/example/drugbank/ui/drugstore/DrugStoreFragment.kt
@@ -1,5 +1,6 @@
 package com.example.drugbank.ui.drugstore
 
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import androidx.lifecycle.ViewModelProvider
@@ -11,6 +12,8 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.example.drugbank.R
+import com.example.drugbank.databinding.FragmentDrugStoreBinding
+import com.example.drugbank.ui.activity.map.MapsActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -22,16 +25,27 @@ import com.google.android.gms.maps.model.MarkerOptions
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class DrugStoreFragment : Fragment() , OnMapReadyCallback{
+class DrugStoreFragment : Fragment() {
 
 
     private lateinit var viewModel: DrugStoreViewModel
-    private lateinit var map: GoogleMap
+    private lateinit var _binding: FragmentDrugStoreBinding
+
+   // private lateinit var map: GoogleMap
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_drug_store, container, false)
+
+       _binding = FragmentDrugStoreBinding.inflate(inflater, container, false)
+
+       _binding.changeToMap.setOnClickListener {
+           val intent = Intent(requireContext(), MapsActivity::class.java)
+           startActivity(intent)
+       }
+
+        return _binding.root
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -43,26 +57,26 @@ class DrugStoreFragment : Fragment() , OnMapReadyCallback{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
-        if (mapFragment != null) {
-            mapFragment.getMapAsync { googleMap ->
-                map = googleMap
-                val hoChiMinhLatLng = LatLng(10.762622, 106.660172 )
-                map.addMarker(MarkerOptions().position(hoChiMinhLatLng).title("What"))
-                map.animateCamera(CameraUpdateFactory.newLatLngZoom(hoChiMinhLatLng, 20F))
-                Log.e("TAG", "Not Null")
-            }
-        } else {
-            Log.e("TAG", "Map fragment is not found in the layout or it is not a SupportMapFragment")
-        }
+//         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+//        if (mapFragment != null) {
+//            mapFragment.getMapAsync { googleMap ->
+//                map = googleMap
+//                val hoChiMinhLatLng = LatLng(10.762622, 106.660172 )
+//                map.addMarker(MarkerOptions().position(hoChiMinhLatLng).title("What"))
+//                map.animateCamera(CameraUpdateFactory.newLatLngZoom(hoChiMinhLatLng, 20F))
+//                Log.e("TAG", "Not Null")
+//            }
+//        } else {
+//            Log.e("TAG", "Map fragment is not found in the layout or it is not a SupportMapFragment")
+//        }
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        map = googleMap
-        val hoChiMinhLatLng = LatLng(0.0, 0.0 )
-        map.addMarker(MarkerOptions().position(hoChiMinhLatLng).title("What"))
-        map.animateCamera(CameraUpdateFactory.newLatLngZoom(hoChiMinhLatLng, 20F))
-    }
+//    override fun onMapReady(googleMap: GoogleMap) {
+//        map = googleMap
+//        val hoChiMinhLatLng = LatLng(0.0, 0.0 )
+//        map.addMarker(MarkerOptions().position(hoChiMinhLatLng).title("What"))
+//        map.animateCamera(CameraUpdateFactory.newLatLngZoom(hoChiMinhLatLng, 20F))
+//    }
 
 
 }
