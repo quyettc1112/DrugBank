0ba7a44d53d8806f78f551d79e519887ed1e36c8
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-03-18 15:41:15 +0700

    Fix User DOB

--- app/src/main/java/com/example/drugbank/ui/setting/SettingViewModel.kt
+++ app/src/main/java/com/example/drugbank/ui/setting/SettingViewModel.kt
@@ -1,10 +1,27 @@
 package com.example.drugbank.ui.setting
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import java.text.SimpleDateFormat
+import java.util.Calendar
 import javax.inject.Inject
 
 @HiltViewModel
 class SettingViewModel @Inject constructor() : ViewModel() {
 
+    private var calendar = Calendar.getInstance()
+    private val dateFormat = SimpleDateFormat("yyyy-MM-dd")
+    val dob = MutableLiveData<String>()
+    init {
+
+        dob.value = dateFormat.format(calendar.time)
+
+
+
+    }
+    fun updateDOBValue(string: String) {
+        dob.value = string
+
+    }
 }
