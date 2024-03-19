9708a8eedbd960ee7975206bfa33027ace313312
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-03-19 23:29:00 +0700

    Update UI cho xinh hơn một xíu, chắc v :)

--- app/src/main/java/com/example/drugbank/ui/record/RecordDetail/RecordDetailViewModel.kt
+++ app/src/main/java/com/example/drugbank/ui/record/RecordDetail/RecordDetailViewModel.kt
@@ -1,7 +1,18 @@
 package com.example.drugbank.ui.record.RecordDetail
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class RecordDetailViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class RecordDetailViewModel @Inject constructor() : ViewModel() {
+
+    private var _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean>
+        get() = _isLoading
+    fun setLoading(isLoading: Boolean) {
+        _isLoading.value = isLoading
+    }
 }
