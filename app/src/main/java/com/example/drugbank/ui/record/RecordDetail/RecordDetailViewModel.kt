3f7f4090eb03db31e286e6ab15d53c4e930fc61e
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-03-20 21:14:06 +0700

    hazzz tôi stress quá

--- app/src/main/java/com/example/drugbank/ui/record/RecordDetail/RecordDetailViewModel.kt
+++ app/src/main/java/com/example/drugbank/ui/record/RecordDetail/RecordDetailViewModel.kt
@@ -3,16 +3,25 @@ package com.example.drugbank.ui.record.RecordDetail
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.drugbank.data.model.Section
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlin.random.Random
 
-@HiltViewModel
-class RecordDetailViewModel @Inject constructor() : ViewModel() {
+
+class RecordDetailViewModel: ViewModel() {
 
     private var _isLoading = MutableLiveData<Boolean>()
+
+
+
     val isLoading: LiveData<Boolean>
         get() = _isLoading
     fun setLoading(isLoading: Boolean) {
         _isLoading.value = isLoading
     }
+
+
+
+
 }
