4f5bb80328025c8e009f4e2f411157016a7eb625
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-03-20 21:13:44 +0700

    hazzz tôi stress quá

---
+++ app/src/main/java/com/example/drugbank/ui/record/RecordDetail/ChildeItem.kt
@@ -0,0 +1,8 @@
+package com.example.drugbank.ui.record.RecordDetail
+
+import com.example.drugbank.respone.ProfileDetailRespone
+
+data class ChildeItem(
+    val productResponseDTO: ProfileDetailRespone.ProfileDetail.ProductResponseDTO
+) {
+}
